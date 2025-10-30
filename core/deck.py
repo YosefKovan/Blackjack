64b244a029a231e636109ac9ef7237760a7f5caf
Yossi Kovan <yossikovan@gmail.com>
2025-10-30 10:21:48 +0200

    adding the shuffling logic functions

--- core/deck.py
+++ core/deck.py
@@ -1,4 +1,66 @@
+import random
 
+
+
+#=============================== shuffling ================================
+
+#======================================
+#          satisfies condition
+#======================================
+def satisfies_condition(deck, index2, suit):
+    """this check if the index satisfies the condition"""
+
+    match suit:
+        case 'H':
+            return True if index2 % 5 == 0 else False
+        case 'C':
+            return True if index2 % 3 == 0 else False
+        case 'D':
+            return True if index2 % 2 == 0 else False
+        case 'S':
+            return True if index2 % 7 == 0 else False
+
+    return False
+
+#======================================
+#        get second swap index
+#======================================
+def get_second_swap_index(deck, index1, suit):
+    """this function will return the second index according to the condition"""
+    print("in the get_second_swap_index")
+    while True:
+        index2 = random.randrange(0, 52)
+
+        #satisfies_condition(deck, index2, deck[index2]["suit"])
+
+        #this will check if the indexes equal
+        if index2 != index1:
+            return index2
+
+
+#======================================
+#           shuffle by suit
+#======================================
+def shuffle_by_suit(deck: list[dict], swaps: int = 5000) -> list[dict]:
+    """this function is in charge of shuffling the deck"""
+    print("in the shuffle_by_suit")
+    shuffle_count = 0
+
+    while shuffle_count < swaps:
+        index1 = random.randrange(0, 52)
+        first_card = deck[index1]
+
+        index2 = get_second_swap_index(deck, index1, first_card["suit"])
+
+        #this will swap between the two indexes
+        deck[index1], deck[index2] = deck[index2], deck[index1]
+        print(shuffle_count)
+        shuffle_count += 1
+
+    return deck
+
+
+#=============================== init deck ================================
 
 #======================================
 #             create card
